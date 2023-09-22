7f0c8b926d12407ae826cde804c4b04491ca0c4a
Abhishek <abhishek.arora1996@gmail.com>
2023-09-22 16:51:03 -0400

    cleaniup for a 0.8.0 release

--- src/linktransformer/modelling/CLFLinkTransformer.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForSequenceClassification
-from typing import Optional, Union, Iterable
-
-##Define methods for 1) Saving model card with LT details, 2)Upload to huggingface (model + tokenizer)
-class CLFLinkTransformer(AutoModelForSequenceClassification):
-    """Modified Sequence Classification model. """
-    pass
